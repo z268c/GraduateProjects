5a81568ebb0f0bf148920020f7afe12d8d145730
z268c <76858023+z268c@users.noreply.github.com>
2024-09-01 09:46:28 +0800

    Delete vue directory

--- vue/src/config.js
+++
@@ -1,8 +0,0 @@
-var setting = require('./setting')
-
-const config = {
-    service_url:'',
-}
-
-
-export default config
--- vue/src/setting.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = {
-    proxyUrl:'http://localhost:8088',
-    title:"旅游管理系统设计与实现",
-}
